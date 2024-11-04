c84a44f16e6378fa425da6ca768b827ca148cbce
JohannFlor <159105070+JohannFlor@users.noreply.github.com>
2024-11-04 03:32:27 -0500

    advances

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -15,7 +15,7 @@ const Movie = sequelize.define('movie', {
         allowNull: false
     },
     releaseYear: {
-        type: DataTypes.ENT,
+        type: DataTypes.INTEGER,
         allowNull: false
     },
 });
